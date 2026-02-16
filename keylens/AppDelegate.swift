666792984d7ec8102960ea51b5304d05a76427ee
Jskckt <joel.skoghammar@scania.com>
2026-02-16 19:53:21 +0100

    Initial Commit

---
+++ keylens/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  keylens
+//
+//  Created by Joel Skoghammar on 2026-02-16.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
---
+++ keylens/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  keylens
+//
+//  Created by Joel Skoghammar on 2026-02-16.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
